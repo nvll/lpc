888eb2090527ea9446f74d2c4fd224c761a675c1
Chris Anderson <chris@nullcode.org>
2012-06-09 02:29:51 -0700

    Brute force the LED bank on

--- main.c
+++ main.c
@@ -6,6 +6,18 @@ void _start (void)
 {
 	SystemInit();
 
+    LPC_IOCON->PIO3_3 = 0xFF;
+    LPC_IOCON->PIO3_2 = 0xFF;
+    LPC_IOCON->PIO3_1 = 0xFF;
+    LPC_IOCON->PIO3_0 = 0xFF;
+    LPC_IOCON->PIO2_4 = 0xFF;
+    LPC_IOCON->PIO2_5 = 0xFF;
+    LPC_IOCON->PIO2_6 = 0xFF;
+    LPC_IOCON->PIO2_7 = 0xFF;
+    LPC_GPIO1->DIR = 0xFF; 
+    LPC_GPIO1->DIR = 0xFF; 
+    LPC_GPIO2->DATA = 0xFF;
+    LPC_GPIO2->DATA = 0xFF;
     LPC_GPIO3->DIR = 0xFF; 
     LPC_GPIO3->DATA = 0xFF;
 
